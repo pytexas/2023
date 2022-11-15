c67fd385482c2e8d047fc600ae907b69990f5d5b
Laura Santamaria <laura@nimbinatus.com>
2022-11-14 21:05:52 -0600

    feat(cname): update siteurl and cname

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://pytexas.org"
+SITEURL = "https://www.pytexas.org"
 RELATIVE_URLS = False
 SITELOGO = SITEURL + "/static/favicon.png"
 FAVICON = SITEURL + "/static/favicon.ico"
